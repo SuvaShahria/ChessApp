2d5f405f609359862530433e3b86d1206566fc12
afkpuezo <andrewfcurry@gmail.com>
2020-12-07 18:04:00 -0500

    fleshed out (first draft of) User.java

--- src/main/java/com/revature/model/User.java
+++ src/main/java/com/revature/model/User.java
@@ -1,11 +1,16 @@
 /**
- *  This class represents/models user accounts for the application.
+ * This class represents/models user accounts for the application.
+ * NOTE: User objects do not store/remember their own passwords. The DAO class/object
+ * performs password checking.
  * 
+ * @author Andrew Curry
  */
 package com.revature.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -22,7 +27,7 @@ public class User{
     public enum UserType {
         NONE,
         PLAYER,
-        ADMIN,
+        ADMIN, // necessary?
     }
 
     // ---------------------
@@ -33,4 +38,83 @@ public class User{
     @Column(name="id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name="USER_TYPE")
+    @Enumerated(EnumType.ORDINAL)
+    private UserType type;
+
+    @Column(name="USERNAME")
+    private String username;
+
+    @Column(name="EMAIL")
+    private String email;
+
+    // ---------------------
+    // CONSTRUCTOR(S)
+    // ---------------------
+
+    public User(){}
+
+    public User(int id, UserType type, String username, String email){
+        this.id = id;
+        this.type = type;
+        this.username = username;
+        this.email = email;
+    }
+
+    public User(UserType type, String username, String email){
+        this.type = type;
+        this.username = username;
+        this.email = email;
+    }
+
+    // ---------------------
+    // SETTERS AND GETTERS
+    // ---------------------
+    
+    public int getId() {
+        return this.id;
+    }
+
+    /**
+     * Does not validate the ID.
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public UserType getType() {
+        return this.type;
+    }
+
+    public void setType(UserType type) {
+        this.type = type;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    /**
+     * Does not validate the username.
+     * 
+     * @param username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    /**
+     * Does not validate the email.
+     * 
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
