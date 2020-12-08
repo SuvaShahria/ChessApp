ee36c2e5d6e2786096a665f40a2525fa41fe5f9a
afkpuezo <andrewfcurry@gmail.com>
2020-12-08 11:45:36 -0500

    (tentatively) done with MatchRecord.java

--- src/main/java/com/revature/model/MatchRecord.java
+++ src/main/java/com/revature/model/MatchRecord.java
@@ -11,6 +11,8 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -60,10 +62,174 @@ public class MatchRecord implements Serializable{
     @JoinColumn(name="CURRENT_TURN_USER_ID", nullable = false, referencedColumnName = "ID")
     private User currentTurnUser;
 
+    @Column(name="MATCH_STATUS")
+    @Enumerated(EnumType.ORDINAL)
+    private MatchStatus status;
+
     @Column(name="MOVE_HISTORY")
     private String moveHistory;
 
     // ---------------------
     // CONSTRUCTOR(S)
     // ---------------------
+
+    /**
+     * Does NOT intialize any fields.
+     */
+    public MatchRecord(){}
+
+    /**
+     * Does NOT validate any fields.
+     */
+    public MatchRecord(
+            int id, 
+            User whiteUser, 
+            User blackUser, 
+            User currentTurnUser, 
+            MatchStatus status,
+            String moveHistory) {
+        this.id = id;
+        this.whiteUser = whiteUser;
+        this.blackUser = blackUser;
+        this.currentTurnUser = currentTurnUser;
+        this.status = status;
+        this.moveHistory = moveHistory;
+    }
+
+    /**
+     * Does NOT validate any fields.
+     */
+    public MatchRecord(
+            User whiteUser, 
+            User blackUser, 
+            User currentTurnUser, 
+            MatchStatus status,
+            String moveHistory) {
+        this.whiteUser = whiteUser;
+        this.blackUser = blackUser;
+        this.currentTurnUser = currentTurnUser;
+        this.status = status;
+        this.moveHistory = moveHistory;
+    }
+
+    // ---------------------
+    // SETTERS AND GETTERS
+    // ---------------------
+
+    public int getId() {
+        return this.id;
+    }
+
+    /**
+     * Does not validate the ID.
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getWhiteUser() {
+        return this.whiteUser;
+    }
+
+    /**
+     * Does not validate the user.
+     * @param whiteUser
+     */
+    public void setWhiteUser(User whiteUser) {
+        this.whiteUser = whiteUser;
+    }
+
+    public User getBlackUser() {
+        return this.blackUser;
+    }
+
+    /**
+     * Does not validate the user.
+     * @param blackUser
+     */
+    public void setBlackUser(User blackUser) {
+        this.blackUser = blackUser;
+    }
+
+    public User getCurrentTurnUser() {
+        return this.currentTurnUser;
+    }
+
+    /**
+     * Does not validate the user.
+     * @param currentTurnUser
+     */
+    public void setCurrentTurnUser(User currentTurnUser) {
+        this.currentTurnUser = currentTurnUser;
+    }
+
+    public MatchStatus getStatus() {
+        return this.status;
+    }
+
+    /**
+     * Does not validate the status.
+     * @param status
+     */
+    public void setStatus(MatchStatus status) {
+        this.status = status;
+    }
+
+    /**
+     * Returns a string containing the move history of this match, in standard (algebraic)
+     * chess notaton. Each move is seperated by a comma. A game with no moves yet will
+     * produce the empty string.
+     * 
+     * EG, "e4 e5,a2 a1,e5 e8"
+     * 
+     * @return
+     */
+    public String getMoveHistory() {
+        return this.moveHistory;
+    }
+
+    /**
+     * Does not validate the history.
+     * @param moveHistory
+     */
+    public void setMoveHistory(String moveHistory) {
+        this.moveHistory = moveHistory;
+    }
+
+    // ---------------------
+    // OTHER PUBLIC METHODS
+    // ---------------------
+
+    /**
+     * Records a single move, adding it to the move history. Also, switches the
+     * currentTurnUser to the other, eg from white to black.
+     * 
+     * The move string should be a single turn/move, without commas, eg "e4 e5"
+     * Does NOT validate the new move string.
+     * 
+     * @param move
+     */
+    public void recordMove(String move){
+        recordMove(move, true);
+    }
+
+    /**
+     * Records a single move, adding it to the move history. Also, if the parameter
+     * changeCurrentTurnUser is true, switches the current player.
+     * 
+     * The move string should be a single turn/move, without commas, eg "e4 e5"
+     * Does NOT validate the new move string.
+     * 
+     * @param move
+     * @param changeCurrentTurnUser
+     */
+    public void recordMove(String move, boolean changeCurrentTurnUser){
+        moveHistory = moveHistory + "," + move;
+        if (changeCurrentTurnUser){
+            if (currentTurnUser.getId() == whiteUser.getId())
+                setCurrentTurnUser(blackUser);
+            else setCurrentTurnUser(whiteUser);
+        }
+    }
 }
