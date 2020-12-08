2f6981478c906ca49558a0a35f4b86c61d20c665
afkpuezo <andrewfcurry@gmail.com>
2020-12-08 12:03:12 -0500

    started MatchRecordRepository, also tweaked doc string in UserRepository

---
+++ src/main/java/com/revature/repository/interfaces/MatchRecordRepository.java
@@ -0,0 +1,11 @@
+/**
+ * This interface specifies the methods for DAO functions related to the MatchRecord 
+ * class/table.
+ * 
+ * @author Andrew Curry
+ */
+package com.revature.repository.interfaces;
+
+public class MatchRecordRepository {
+    
+}
--- src/main/java/com/revature/repository/interfaces/UserRepository.java
+++ src/main/java/com/revature/repository/interfaces/UserRepository.java
@@ -1,6 +1,7 @@
 /**
  * This interface specifies the methods for DAO functions related to the User class/table.
  * 
+ * @author Andrew Curry
  */
 package com.revature.repository.interfaces;
 
