8b74acec78c57f51d4665d65c61ce210798e5977
SuvaShahria <1uknow2@gmail.com>
2020-12-09 14:29:00 -0500

    z

--- chessAppNg/src/app/app.component.ts
+++ chessAppNg/src/app/app.component.ts
@@ -1,25 +1,68 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import * as ChessBoard from 'chessboardjs/www/js/chessboard';
-import * as Chess from 'chessboardjs/www/js/chess'
+import  Chess from 'chess.js';
+//declare const Chess:any;
+ //const Chess = require('chess.js');
+ //import * as Chess from 'chessboardjs/www/js/chess.js'
+ 
+//declare const Chess:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
-
-  title = 'frontend';
-  board1: ChessBoard;
+export class AppComponent implements OnInit {
+  public chess: any ;
+ 
+  board: ChessBoard;
+   //chess = new Chess();
   ngOnInit(){
-    this.board1 = ChessBoard('board1', {
+    this.chess = new Chess();
+    //this.game = new Chess.Chess();
+    this.board = ChessBoard('board', {
       draggable: true,
-      dropOffBoard: 'trash',
-      sparePieces: true
+        position: 'start',
+        pieceTheme: './assets/img/chesspieces/wikipedia/{piece}.png',
+        onDragStart: this.onDragStart,
+        onDrop: this.onDrop,
+        onSnapEnd: this.onSnapEnd
     })
   }
-  ngAfterViewInit() {
+  
+  // should return false if not ur turn
+  //source -starting position/ piece name/ position/ where to
+  public onDragStart (source, piece, position, orientation): boolean{
+
+    //game end
     
+    //this.chess.game_over()
+
+     // console.log(this.chess.game_over())
+    // only pick up pieces for the side to move
+   // console.log(this.chess.turn())
     
 
-}
+    return true;
+  }
+
+  //If move legal return empty string. else return string 'snapback'
+  public onDrop (source, target): String{
+    // var move = this.chess.move({
+    //   from: source,
+    //   to: target,
+    //   promotion: 'q' //promote to queen
+    // })
+  
+    // illegal move
+    // if (move === null) return 'snapback'
+    return ''
+    //to revert return 'snapback'
+  }
+
+  public onSnapEnd (){
+   // this.board.position(this.game.fen())
+  }
+
+  
+  
 }
