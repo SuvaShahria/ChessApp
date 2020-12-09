cacf60d697901eb2235f19a7f4433b04c74baa9e
SuvaShahria <1uknow2@gmail.com>
2020-12-09 15:24:09 -0500

    w

--- chessAppNg/src/app/app.component.ts
+++ chessAppNg/src/app/app.component.ts
@@ -12,57 +12,13 @@ import  Chess from 'chess.js';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  public chess: any ;
- 
-  board: ChessBoard;
-   //chess = new Chess();
+  
+  //@input document.getElementById('board').style.visibility = 'visible';
   ngOnInit(){
-    this.chess = new Chess();
-    //this.game = new Chess.Chess();
-    this.board = ChessBoard('board', {
-      draggable: true,
-        position: 'start',
-        pieceTheme: './assets/img/chesspieces/wikipedia/{piece}.png',
-        onDragStart: this.onDragStart,
-        onDrop: this.onDrop,
-        onSnapEnd: this.onSnapEnd
-    })
+    
   }
   
-  // should return false if not ur turn
-  //source -starting position/ piece name/ position/ where to
-  public onDragStart (source, piece, position, orientation): boolean{
-
-    //game end
-    
-    //this.chess.game_over()
-
-     // console.log(this.chess.game_over())
-    // only pick up pieces for the side to move
-   // console.log(this.chess.turn())
-    
-
-    return true;
-  }
-
-  //If move legal return empty string. else return string 'snapback'
-  public onDrop (source, target): String{
-    // var move = this.chess.move({
-    //   from: source,
-    //   to: target,
-    //   promotion: 'q' //promote to queen
-    // })
   
-    // illegal move
-    // if (move === null) return 'snapback'
-    return ''
-    //to revert return 'snapback'
-  }
-
-  public onSnapEnd (){
-   // this.board.position(this.game.fen())
-  }
-
   
   
 }
