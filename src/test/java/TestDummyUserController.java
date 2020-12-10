8e229af2e97bc05c7ea9d6be70ad48049181306c
afkpuezo <andrewfcurry@gmail.com>
2020-12-10 15:40:23 -0500

    added junit and mockito dependencies to pom

---
+++ src/test/java/TestDummyUserController.java
@@ -0,0 +1,12 @@
+/**
+ * This class contains unit tests for the first, incomplete implementation of the
+ * UserController interface. When the actual implementation is developed, these tests
+ * should be discarded/excluded.
+ * 
+ * @author Andrew Curry
+ */
+
+public class TestDummyUserController {
+    
+    @Before 
+}
