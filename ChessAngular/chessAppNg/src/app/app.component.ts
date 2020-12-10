eaca7d5aa488fd56840fd6809cc66dd819f90966
SuvaShahria <1uknow2@gmail.com>
2020-12-10 18:03:59 -0500

    in componenent

--- ChessAngular/chessAppNg/src/app/app.component.ts
+++ ChessAngular/chessAppNg/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as ChessBoard from 'chessboardjs/www/js/chessboard';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'chessAppNg';
+  board: ChessBoard;
+  
+  //$status = $('#status')
+  ngAfterViewInit() {
+    this.board = ChessBoard('board', {
+      draggable: true,
+        position: 'start',
+        pieceTheme: './assets/img/chesspieces/wikipedia/{piece}.png',
+       
+         onDragStart: this.onDragStart,
+         onDrop: this.onDrop,
+         onSnapEnd: this.onSnapEnd
+    })
+  }
+
+  // logic in main branch of index.html
+  public onDragStart (source, piece, position, orientation) :boolean{
+    return true;
+  }
+
+  public onDrop (source, target): String{
+    return '';
+  }
+
+  public onSnapEnd (): void{
+
+  }
 }
