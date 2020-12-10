91b26361b686e24f1adbb2d93d31e08ea235120e
afkpuezo <andrewfcurry@gmail.com>
2020-12-10 11:25:12 -0500

    added some files and folders, based on SpingMVCAngular demo

---
+++ src/main/java/com/revature/util/ClientMessageUtil.java
@@ -0,0 +1,15 @@
+/**
+ * This is taken from the SpringMVCAngular demo from class.
+ * 
+ * @author Andrew Curry (well, really it's Sophia Gavrila)
+ */
+package com.revature.util;
+
+import com.revature.ajax.ClientMessage;
+
+public class ClientMessageUtil {
+	public static final ClientMessage REGISTRATION_SUCCESSFUL = new ClientMessage("REGISTRATION SUCCESSFUL");
+
+	public static final ClientMessage SOMETHING_WRONG = new ClientMessage("SOMETHING WENT WRONG");
+
+}
