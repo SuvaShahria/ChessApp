b502554ebffbd0de8fa7e0c3da69371b49bdb59b
afkpuezo <andrewfcurry@gmail.com>
2020-12-10 15:54:19 -0500

    started testing UserController

--- src/test/java/TestDummyUserController.java
+++ src/test/java/TestDummyUserController.java
@@ -1,3 +1,17 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.revature.ajax.ClientMessage;
+import com.revature.model.User;
+import com.revature.model.User.UserType;
+import com.revature.rest.impl.UserControllerImpl;
+import com.revature.rest.interfaces.UserController;
+import com.revature.util.ClientMessageUtil;
+
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * This class contains unit tests for the first, incomplete implementation of the
  * UserController interface. When the actual implementation is developed, these tests
@@ -8,5 +22,30 @@
 
 public class TestDummyUserController {
     
-    @Before 
+    // ---------------------
+    // CLASS-LEVEL VARIABLES
+    // ---------------------
+    private static UserController ucon;
+
+    // ---------------------
+    // SUPPORT METHODS
+    // ---------------------
+
+    @Before
+    public void setup(){
+        ucon = new UserControllerImpl();
+    }
+
+    // ---------------------
+    // TESTS
+    // ---------------------
+
+    @Test
+    public void testRegisterUser(){
+        User newguy = new User(UserType.PLAYER, "newguy", "new@email.com");
+        ClientMessage cm = ucon.registerUser(newguy, "pass");
+        assertEquals(
+                ClientMessageUtil.REGISTRATION_SUCCESSFUL.getMessage(), 
+                cm.getMessage());
+    }
 }
