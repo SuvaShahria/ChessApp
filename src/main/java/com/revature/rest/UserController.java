6eb811a57fba72464c2bd20edcbba6138e2b028b
afkpuezo <andrewfcurry@gmail.com>
2020-12-10 14:34:21 -0500

    fixed ViewControllerImpl (from interface to class), started UserController, added servlet and jackson dependencies to maven

---
+++ src/main/java/com/revature/rest/UserController.java
@@ -0,0 +1,14 @@
+/**
+ * This is taken from the SpringMVCAngular demo from class.
+ * 
+ * @author Andrew Curry (well, really it's Sophia Gavrila)
+ */
+package com.revature.rest;
+
+
+import javax.servlet.http.HttpServletRequest;
+import com.revature.ajax.ClientMessage;
+
+public class UserController {
+    
+}
--- src/main/java/com/revature/view/ViewControllerImpl.java
+++ src/main/java/com/revature/view/ViewControllerImpl.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller("viewController") // the @Controller this marks the class as a ewb request handler
-public interface ViewControllerImpl {
+public class ViewControllerImpl implements ViewController{
     
     @RequestMapping(value = { "/", "/index"}, method = RequestMethod.GET)
 	public String index() {
