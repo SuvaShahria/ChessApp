403afd4ca2a190050288c23c698a685d64f74910
Matthew <>
2020-12-10 17:31:43 -0600

    Add User model,service,register
    Setup all of the registration part. The USER_URL in the environment needs to have to correct URL going to backend. not sure what that value is.

--- ChessAngular/chessAppNg/src/app/components/register/register.component.ts
+++ ChessAngular/chessAppNg/src/app/components/register/register.component.ts
@@ -1,15 +1,31 @@
+import { UserService } from './../../services/user.service';
+import { ClientMessage } from './../../models/client-message.model';
 import { Component, OnInit } from '@angular/core';
+import { User } from './../../models/user.model';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  
+  title = 'Register New Player';
+  
+  // Constructor Injection
+  constructor(private userService: UserService) { }
 
-  constructor() { }
+  //For databinding
+  public user: User = new User('', '', '', '', '');
 
-  ngOnInit(): void {
+  // Message to the user
+  public clientMessage: ClientMessage = new ClientMessage('');
+
+  public registerHero(): void {
+    this.userService.registerHero(this.user)
+      .subscribe(
+        data => this.clientMessage = data,
+        error => this.clientMessage.message = 'SOMETHING WENT WRONG'
+      )
   }
-
 }
