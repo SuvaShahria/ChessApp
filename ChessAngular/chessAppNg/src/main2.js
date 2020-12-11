3614a194dd3209d4bf0d57f4cbdd457715c8d8a6
SuvaShahria <1uknow2@gmail.com>
2020-12-10 23:55:40 -0500

    added grey square

--- ChessAngular/chessAppNg/src/main2.js
+++ ChessAngular/chessAppNg/src/main2.js
@@ -10,10 +10,48 @@ var myExtObject = (function() {
       
     // document.getElementById('board').style.visibility = 'visible';
     pieces =s;
+    var whiteSquareGrey = '#a9a9a9'
+    var blackSquareGrey = '#696969'
     var board = null;
     var game = new Chess()
     var $status = $('#status')
     var $mvDiv = $('#mvDiv')  
+    function removeGreySquares () {
+      $('#board .square-55d63').css('background', '')
+    }
+    function greySquare (square) {
+      var $square = $('#board .square-' + square)
+    
+      var background = whiteSquareGrey
+      if ($square.hasClass('black-3c85d')) {
+        background = blackSquareGrey
+      }
+    
+      $square.css('background', background)
+    }
+    function onMouseoverSquare (square, piece) {
+      // get list of possible moves for this square
+      var moves = game.moves({
+        square: square,
+        verbose: true
+      })
+    
+      // exit if there are no moves available for this square
+      if (moves.length === 0) return
+    
+      // highlight the square they moused over
+      greySquare(square)
+    
+      // highlight the possible squares for this piece
+      for (var i = 0; i < moves.length; i++) {
+        greySquare(moves[i].to)
+      }
+    }
+    
+    function onMouseoutSquare (square, piece) {
+      removeGreySquares()
+    }
+
     function onDragStart (source, piece, position, orientation) {
       //game end
       if (game.game_over()) return false
@@ -26,6 +64,7 @@ var myExtObject = (function() {
     }
     
     function onDrop (source, target) {
+      removeGreySquares()
      
       // see if the move is legal
       var move = game.move({
@@ -95,6 +134,8 @@ var myExtObject = (function() {
       pieceTheme: pieces,
       onDragStart: onDragStart,
       onDrop: onDrop,
+      onMouseoutSquare: onMouseoutSquare,
+      onMouseoverSquare: onMouseoverSquare,
       onSnapEnd: onSnapEnd
   })
 
