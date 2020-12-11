cd4b1f5eda555f88dce5ed1f42a3e3c46ec2b7e8
SuvaShahria <1uknow2@gmail.com>
2020-12-10 21:03:38 -0500

    properly commented out

--- ChessAngular/chessAppNg/src/main2.js
+++ ChessAngular/chessAppNg/src/main2.js
@@ -1,78 +1,78 @@
 
-    // document.getElementById('board').style.visibility = 'visible';
-      var board = null;
-     var game = new Chess()
-     var $status = $('#status')
+  //   // document.getElementById('board').style.visibility = 'visible';
+  //     var board = null;
+  //    var game = new Chess()
+  //    var $status = $('#status')
 
-     function onDragStart (source, piece, position, orientation) {
-       //game end
-       if (game.game_over()) return false
+  //    function onDragStart (source, piece, position, orientation) {
+  //      //game end
+  //      if (game.game_over()) return false
      
-       // only pick up pieces for the side to move
-       if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-           (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-         return false
-       }
-     }
+  //      // only pick up pieces for the side to move
+  //      if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+  //          (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+  //        return false
+  //      }
+  //    }
      
-     function onDrop (source, target) {
+  //    function onDrop (source, target) {
       
-       // see if the move is legal
-       var move = game.move({
-         from: source,
-         to: target,
-         promotion: 'q' //promote to queen
-       })
+  //      // see if the move is legal
+  //      var move = game.move({
+  //        from: source,
+  //        to: target,
+  //        promotion: 'q' //promote to queen
+  //      })
      
-       // illegal move
-       if (move === null) return 'snapback'
+  //      // illegal move
+  //      if (move === null) return 'snapback'
      
-       updateStatus()
-     }
+  //      updateStatus()
+  //    }
      
-     // update the board position after the piece snap
-     function onSnapEnd () {
-       board.position(game.fen())
-     }
+  //    // update the board position after the piece snap
+  //    function onSnapEnd () {
+  //      board.position(game.fen())
+  //    }
      
-     function updateStatus () {
-       var status = ''
+  //    function updateStatus () {
+  //      var status = ''
      
-       var moveColor = 'White'
-       if (game.turn() === 'b') {
-         moveColor = 'Black'
-       }
+  //      var moveColor = 'White'
+  //      if (game.turn() === 'b') {
+  //        moveColor = 'Black'
+  //      }
      
-       // checkmate
-       if (game.in_checkmate()) {
-         status = 'Game over, ' + moveColor + ' is in checkmate.'
-       }
+  //      // checkmate
+  //      if (game.in_checkmate()) {
+  //        status = 'Game over, ' + moveColor + ' is in checkmate.'
+  //      }
      
-       // draw?
-       else if (game.in_draw()) {
-         status = 'Game over, drawn position'
-       }
+  //      // draw?
+  //      else if (game.in_draw()) {
+  //        status = 'Game over, drawn position'
+  //      }
      
        
-       else {
-         status = moveColor + ' turn'
+  //      else {
+  //        status = moveColor + ' turn'
      
          
-         if (game.in_check()) {
-           status += ', ' + moveColor + ' is in check'
-         }
-       }
+  //        if (game.in_check()) {
+  //          status += ', ' + moveColor + ' is in check'
+  //        }
+  //      }
      
-       $status.html(status)
+  //      $status.html(status)
        
-     }
-     board = ChessBoard('board', {
-     draggable: true,
-       position: 'start',
-       pieceTheme: './assets/img/chesspieces/wikipedia/{piece}.png',
-       onDragStart: onDragStart,
-       onDrop: onDrop,
-       onSnapEnd: onSnapEnd
-   })
+  //    }
+  //    board = ChessBoard('board', {
+  //    draggable: true,
+  //      position: 'start',
+  //      pieceTheme: './assets/img/chesspieces/wikipedia/{piece}.png',
+  //      onDragStart: onDragStart,
+  //      onDrop: onDrop,
+  //      onSnapEnd: onSnapEnd
+  //  })
 
-   updateStatus()
+  //  updateStatus()
