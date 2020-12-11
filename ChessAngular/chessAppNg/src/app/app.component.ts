45d4d2efa967afa72f333df8200472a89eee7514
SuvaShahria <1uknow2@gmail.com>
2020-12-10 20:59:27 -0500

    save

--- ChessAngular/chessAppNg/src/app/app.component.ts
+++ ChessAngular/chessAppNg/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+declare var myExtObject: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'chessAppNg';
+  ngOnInit(): void{
+    this.createGame()
+  }
+
+  ngOnDestroy(){
+    
+  }
+  createGame() {
+    console.log("hi")
+    myExtObject.create();
+  }
+
+  
+
+  callFunction2() {
+      console.log(myExtObject.func2());
+  }
+
+  delete(){
+    myExtObject.delete();
+    
+  }
+
+  add(){
+    myExtObject.add();
+  }
 }
--- ChessAngular/chessAppNg/src/main2.js
+++ ChessAngular/chessAppNg/src/main2.js
@@ -1,78 +1,119 @@
+var myExtObject = (function() {
+  var t = 0;
 
-    // document.getElementById('board').style.visibility = 'visible';
-      var board = null;
-     var game = new Chess()
-     var $status = $('#status')
-
-     function onDragStart (source, piece, position, orientation) {
-       //game end
-       if (game.game_over()) return false
-     
-       // only pick up pieces for the side to move
-       if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-           (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-         return false
-       }
-     }
-     
-     function onDrop (source, target) {
+  return {
+    create: function() {
+      // ---------------------------------------------------------------------------------------------------
       
-       // see if the move is legal
-       var move = game.move({
-         from: source,
-         to: target,
-         promotion: 'q' //promote to queen
-       })
-     
-       // illegal move
-       if (move === null) return 'snapback'
-     
-       updateStatus()
-     }
-     
-     // update the board position after the piece snap
-     function onSnapEnd () {
-       board.position(game.fen())
-     }
-     
-     function updateStatus () {
-       var status = ''
-     
-       var moveColor = 'White'
-       if (game.turn() === 'b') {
-         moveColor = 'Black'
-       }
-     
-       // checkmate
-       if (game.in_checkmate()) {
-         status = 'Game over, ' + moveColor + ' is in checkmate.'
-       }
-     
-       // draw?
-       else if (game.in_draw()) {
-         status = 'Game over, drawn position'
-       }
-     
-       
-       else {
-         status = moveColor + ' turn'
-     
-         
-         if (game.in_check()) {
-           status += ', ' + moveColor + ' is in check'
-         }
-       }
-     
-       $status.html(status)
-       
-     }
-     board = ChessBoard('board', {
-     draggable: true,
-       position: 'start',
-       pieceTheme: './assets/img/chesspieces/wikipedia/{piece}.png',
-       onDragStart: onDragStart,
-       onDrop: onDrop,
-       onSnapEnd: onSnapEnd
-   })
+    // document.getElementById('board').style.visibility = 'visible';
+    var board = null;
+    var game = new Chess()
+    var $status = $('#status')
 
-   updateStatus()
+    function onDragStart (source, piece, position, orientation) {
+      //game end
+      if (game.game_over()) return false
+    
+      // only pick up pieces for the side to move
+      if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+          (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+        return false
+      }
+    }
+    
+    function onDrop (source, target) {
+     
+      // see if the move is legal
+      var move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q' //promote to queen
+      })
+    
+      // illegal move
+      if(move != null) t = t+1
+      if (move === null) return 'snapback'
+    
+      updateStatus()
+    }
+    
+    // update the board position after the piece snap
+    function onSnapEnd () {
+      board.position(game.fen())
+    }
+    
+    function updateStatus () {
+      var status = ''
+    
+      var moveColor = 'White'
+      if (game.turn() === 'b') {
+        moveColor = 'Black'
+      }
+    
+      // checkmate
+      if (game.in_checkmate()) {
+        status = 'Game over, ' + moveColor + ' is in checkmate.'
+      }
+    
+      // draw?
+      else if (game.in_draw()) {
+        status = 'Game over, drawn position'
+      }
+    
+      
+      else {
+        status = moveColor + ' turn'
+    
+        
+        if (game.in_check()) {
+          status += ', ' + moveColor + ' is in check'
+        }
+      }
+    
+      $status.html(status)
+      
+    }
+    board = ChessBoard('board', {
+    draggable: true,
+      position: 'start',
+      pieceTheme: './assets/img/chesspieces/wikipedia/{piece}.png',
+      onDragStart: onDragStart,
+      onDrop: onDrop,
+      onSnapEnd: onSnapEnd
+  })
+
+  updateStatus()
+
+
+
+
+
+
+      //-------------------------------------------------CREATE------------------------------------------------------------------------
+    },
+    func2: function() {
+      return t;
+    },
+    delete: function(){
+      var b = document.getElementById("board");
+      b.remove()
+      var b1 = document.getElementById("st");
+      b1.remove()
+      var b2 = document.getElementById("status");
+      b2.remove()
+    },
+    add: function(){
+      c = document.createElement('div');
+      c.setAttribute("id", "board");
+      document.body.appendChild(c);
+      c = document.createElement('Label');
+      c.setAttribute("id", "st");
+      c.innerHTML= "STATUS:"
+      document.body.appendChild(c);
+      c = document.createElement('div');
+      c.setAttribute("id", "status");
+      document.body.appendChild(c);
+    }
+  }
+
+})(myExtObject||{})
